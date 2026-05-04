1d09b83f9d874fbb52d8ebf9c8d56ece64fbb2b2
guldmand <guldmand@MacBook-Pro-tilhrende-guldmand.local>
2026-05-04 02:54:21 +0200

    added externals: Gymnasium & ObjectRL

---
+++ external/Gymnasium/tests/wrappers/vector/test_normalize_reward.py
@@ -0,0 +1,95 @@
+"""Test suite for vector NormalizeReward wrapper."""
+
+import numpy as np
+
+from gymnasium import wrappers
+from gymnasium.core import ActType
+from gymnasium.vector import SyncVectorEnv
+from tests.testing_env import GenericTestEnv
+
+
+def reset_func(self, seed: int | None = None, options: dict | None = None):
+    self.step_id = 0
+    return self.observation_space.sample(), {}
+
+
+def step_func(self, action: ActType):
+    self.step_id += 1
+    terminated = self.step_id == 10
+    return self.observation_space.sample(), float(terminated), terminated, False, {}
+
+
+def thunk():
+    return GenericTestEnv(step_func=step_func, reset_func=reset_func)
+
+
+def test_functionality(
+    n_envs=3,
+    n_steps=100,
+):
+    env = SyncVectorEnv([thunk for _ in range(n_envs)])
+    env = wrappers.vector.NormalizeReward(env)
+
+    env.reset()
+    for _ in range(n_steps):
+        action = env.action_space.sample()
+        env.step(action)
+
+    env.reset()
+    forward_rets = []
+    accumulated_rew = 0
+    for _ in range(n_steps):
+        action = env.action_space.sample()
+        _, reward, terminated, truncated, _ = env.step(action)
+        dones = np.logical_or(terminated, truncated)
+        accumulated_rew = accumulated_rew * 0.9 * dones + reward
+        forward_rets.append(accumulated_rew)
+
+    env.close()
+
+    forward_rets = np.asarray(forward_rets)
+    assert np.allclose(np.std(forward_rets, axis=0), 0.89, atol=0.1)
+
+
+def test_against_wrapper(n_envs=3, n_steps=100, rtol=0.1, atol=0):
+    vec_env = SyncVectorEnv([thunk for _ in range(n_envs)])
+    vec_env = wrappers.vector.NormalizeReward(vec_env)
+    vec_env.reset()
+    for _ in range(n_steps):
+        action = vec_env.action_space.sample()
+        vec_env.step(action)
+
+    env = wrappers.Autoreset(thunk())
+    env = wrappers.NormalizeReward(env)
+    env.reset()
+    for _ in range(n_steps):
+        action = env.action_space.sample()
+        env.step(action)
+
+    assert np.allclose(env.return_rms.var, vec_env.return_rms.var, rtol=rtol, atol=atol)
+
+
+def test_equivalence_with_wrapper(n_steps=50):
+    def thunk_with_normalize():
+        return wrappers.NormalizeReward(thunk())
+
+    per_env = SyncVectorEnv([thunk_with_normalize])
+    per_env.reset(seed=42)
+    for _ in range(n_steps):
+        per_env.step(per_env.action_space.sample())
+
+    vec_env = SyncVectorEnv([thunk])
+    vec_env = wrappers.vector.NormalizeReward(vec_env)
+    vec_env.reset(seed=42)
+    for _ in range(n_steps):
+        vec_env.step(vec_env.action_space.sample())
+
+    assert vec_env.return_rms.count == per_env.envs[0].return_rms.count
+    assert np.allclose(
+        vec_env.return_rms.mean, per_env.envs[0].return_rms.mean, rtol=1e-4
+    )
+    assert np.allclose(
+        vec_env.return_rms.var, per_env.envs[0].return_rms.var, rtol=1e-4
+    )
+    per_env.close()
+    vec_env.close()
